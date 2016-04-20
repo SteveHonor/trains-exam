6546f95751f33627baeab80d24de57fcd8632397
Steve J <steve@honors.com.br>
2016-04-20 19:37:54 -0300

    Initialize railroad step definition for BDD Cucumber
    Step Definitons of Features Cucumber

---
+++ features/step_definitions/railroad_step.rb
@@ -0,0 +1,27 @@
+Given(/^that I have the cities "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^compute the distance between cities "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^it should display the following message: "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the number of trips starting at "([^"]*)" and ending at "([^"]*)" with a maximum of (\d+) stops$/) do |arg1, arg2, arg3|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the number of trips starting at A and ending at C with exactly (\d+) stops$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the length of the shortest route between cities "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the number of different routes from C to C with a distance of less than (\d+)$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
