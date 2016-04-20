6a1e36004762db6d12a77a7211e7f3d3fc95c642
Steve J <steve@honors.com.br>
2016-04-20 19:36:56 -0300

    Initialize customers step definition for BDD Cucumber
    Step Definitons of Features Cucumber

---
+++ features/step_definitions/customers_step.rb
@@ -0,0 +1,31 @@
+Given(/^that I'm going to the city "([^"]*)" passing through town "([^"]*)" and stopping in the city "([^"]*)"$/) do |arg1, arg2, arg3|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^entering the cities "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I want to see the route "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^that I'm leaving the city "([^"]*)" and then going to town "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I want to know the "([^"]*)" of travel routes$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^that I'm going from city A to city D, I know the best route$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^entering the city "([^"]*)" and the city "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I want to see the route of "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
